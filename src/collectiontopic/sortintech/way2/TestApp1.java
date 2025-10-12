f952b369b79b9c15400128fab3e538daf57228ea
rahul kirpekar <rahul.kirpekar@gmail.com>
2025-10-12 15:05:05 +0530

    sorting-tech-2

---
+++ src/collectiontopic/sortintech/way2/TestApp1.java
@@ -0,0 +1,55 @@
+package collectiontopic.sortintech.way2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class TestApp1 
+{
+	public static void main(String[] args) 
+	{
+		Student s1= new Student(1, "rahul", 12);
+		Student s2= new Student(2, "ankur", 11);
+		Student s3= new Student(3, "sagar", 10);
+		Student s4= new Student(4, "ramesh", 9);
+		Student s5= new Student(5, "brijesh", 8);
+		
+		ArrayList<Student> list = new ArrayList<>();
+		
+		list.add(s1);
+		list.add(s2);
+		list.add(s3);
+		list.add(s4);
+		list.add(s5);
+		
+		Iterator<Student> itr =	list.iterator();
+		
+		while (itr.hasNext()) 
+		{
+			Student s= (Student) itr.next();
+			s.dispData();
+		}
+		System.out.println("---------------------------------------");
+		
+		Collections.sort(list, new StdWiseStudentComparator());
+		
+		itr =	list.iterator();
+		
+		while (itr.hasNext()) 
+		{
+			Student s= (Student) itr.next();
+			s.dispData();
+		}
+		System.out.println("---------------------------------------");
+
+		Collections.sort(list, new NameWiseStudentComparator());
+		
+		itr =	list.iterator();
+		
+		while (itr.hasNext()) 
+		{
+			Student s= (Student) itr.next();
+			s.dispData();
+		}
+	}
+}
