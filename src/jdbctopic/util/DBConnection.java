c06050bc9805dde0080ee1941eff4b766da6e12e
rahul kirpekar <rahul.kirpekar@gmail.com>
2025-11-01 17:57:39 +0530

    dbconnection

---
+++ src/jdbctopic/util/DBConnection.java
@@ -0,0 +1,52 @@
+package jdbctopic.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection 
+{
+	// 1. Make credentials
+	private static final String URLNAME = "jdbc:mysql://localhost:3306/genweekend24";
+	private static final String DRIVERCLASS      ="com.mysql.cj.jdbc.Driver";
+	private static final String USERNAME         ="root";
+	private static final String PASSWORD			="root";
+
+	// 2. getConnection method
+	public static Connection getConnection() 
+	{
+		Connection conn = null;
+		try 
+		{
+			// 3. Load Driver class
+			Class.forName(DRIVERCLASS);
+
+			// 4. Pass credentials in DriverManager's getConnection method
+			conn = DriverManager.getConnection(URLNAME, USERNAME, PASSWORD);
+
+			// 5. validate connection object
+			if (conn!=null) 
+			{
+				System.out.println("Db connected : " + conn);
+			} else 
+			{
+				System.out.println("Db not connected : " + conn);
+			}
+		} catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		} catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+
+	public static void main(String[] args) 
+	{
+		Connection conn = DBConnection.getConnection(); 
+
+		System.out.println("Main Function : conn --" + conn);
+	}
+}
